92ab5ccbd44818cea3050dc0f65b964bc3c79ab2
st1vms <96783398+st1vms@users.noreply.github.com>
2025-02-03 13:11:48 +0100

    First version!

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
 # ws-reader
-Python Websocket reader based off Selenium and Chromedriver
+
+Python Websocket reader based off Selenium and Chromedriver.
+Able to read inbound and outbound websocket messages in a browser session.
+
+## Requirements
+
+- Windows
+
+- Python >= 3.10
+
+- Latest Chrome version
+
+- Latest stable [chromedriver.exe](https://googlechromelabs.github.io/chrome-for-testing/) inside a PATH folder
+
+- `selenium` package installed
+
+```shell
+pip install selenium
+```
+
+## Example Usage
+
+```py
+# Import listener
+from ws_reader import WSListener
+
+# Create a listener
+listener = WSListener(
+    url='https://www.twitch.tv/',
+    chrome_profile='Default', # Default chrome profile
+    headless=True, # Defaults to headless
+    logging=False # Disable message logging
+)
+
+# Run the listener into a daemon thread
+try:
+    listener.start()
+    # Print first 10 messages and exit
+    for _ in range(10):
+        # Read WebSocketMessage from internal queue
+        print(listener.messages.get())
+finally:
+    # Remember to close the listener thread
+    listener.close()
+```
+
+## WebSocketMessage type
+
+The internal `messages` Queue holds all the messages received and sent over the browser session,
+they are dataclasses of type `WebSocketMessage`.
+
+These are the properties of a message:
+
+-`payload` (str): The content of the WebSocket message.
+
+-`request_id` (str): A unique identifier for the request associated with the message.
+
+-`timestamp` (float): The time at which the message was received, represented as a Unix timestamp.
+
+-`url` (str): The URL of the WebSocket endpoint from which the message was received or sent to.
---
+++ ws_reader.py
@@ -0,0 +1,160 @@
+"""WebSocket listener using selenium and chromedriver"""
+
+from os import path as ospath
+from os import environ
+from dataclasses import dataclass
+from json import loads as json_loads
+from queue import Queue
+from threading import Event, Thread
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+CHROME_USER_DATA_DIR = ospath.join(environ["LOCALAPPDATA"], r"Google\Chrome\User Data")
+
+
+@dataclass(frozen=True)
+class WebSocketMessage:
+    """
+    A dataclass that represents a message received over a WebSocket connection.\n
+    Attributes:
+        `payload` (str): The content of the WebSocket message.
+        `request_id` (str): A unique identifier for the request associated with the message.
+        `timestamp` (float): The time at which the message was received, represented as a Unix timestamp.
+        `url` (str): The URL of the WebSocket endpoint from which the message was received or sent to.
+    """
+
+    payload: str
+    request_id: str
+    timestamp: float
+    url: str
+
+
+class WSListener:
+    """WebSocket listener class"""
+
+    def __init__(
+        self,
+        url: str,
+        chrome_profile: str = "Default",
+        headless: bool = True,
+        queue: Queue[WebSocketMessage] = None,
+        logging: bool = False,
+    ) -> None:
+        """
+        Initializes the WebSocket listener with the given parameters.\n
+        Args:
+            `url` (str): The website URL to open.
+            `chrome_profile` (str, optional): The Chrome profile to use. Defaults to "Default".
+            `headless` (bool, optional): Enable or disable browser headless mode, Defaults to True
+            `queue` (Queue, optional): Optional Queue instance to use instead of internal one
+            `logging` (bool, optional): Enable or disable message logging. Defaults to False.
+
+        This listener provides a Queue property called `messages`, which will contain all the
+        intercepted websocket messages, you can retrieve them with Queue methods like .get(),
+        they will be dataclasses of type WebSocketMessage.
+        """
+        self.url = url
+        self.logging = logging
+
+        self.messages: Queue[WebSocketMessage] = queue or Queue()
+
+        self.driver: webdriver.Chrome = None
+
+        # Set Chrome options
+        self.opts = Options()
+        self.opts.add_argument(f"--user-data-dir={CHROME_USER_DATA_DIR}")
+        self.opts.add_argument(f"--profile-directory={chrome_profile}")
+        if headless:
+            self.opts.add_argument("--headless")
+
+        # Enable performance logging so that we can capture DevTools protocol events.
+        self.opts.set_capability("goog:loggingPrefs", {"performance": "ALL"})
+
+        self.running: Event = Event()
+        self.thread: Thread = None
+
+        self.websocket_url_map = {}
+
+    def _read_loop(self) -> None:
+        """Reads in loop websocket for messages and calls the assigned callback
+        to this listener, passing the message as argument"""
+
+        while self.running.is_set():
+
+            # Retrieve performance logs. Each log entry is a JSON string containing a CDP event.
+            logs = self.driver.get_log("performance")
+
+            # Process logs to find WebSocket frame events.
+            for entry in logs:
+                message = json_loads(entry["message"])["message"]
+                method = message.get("method", "")
+                params = message.get("params", {})
+
+                # Capture the creation of a WebSocket connection.
+                if method == "Network.webSocketCreated":
+                    request_id = params.get("requestId")
+                    ws_url = params.get("url")
+                    if request_id and ws_url:
+                        self.websocket_url_map[request_id] = ws_url
+
+                        if self.logging:
+                            print(
+                                f"WebSocket created: [ID: {request_id}] URL: {ws_url}"
+                            )
+
+                # Process incoming WebSocket messages.
+                elif method == "Network.webSocketFrameReceived":
+                    request_id = params.get("requestId")
+                    payload = message["params"]["response"].get("payloadData", "")
+                    timestamp = message["params"].get("timestamp")
+                    ws_url = self.websocket_url_map.get(request_id, "Unknown URL")
+                    self.messages.put(
+                        WebSocketMessage(payload, request_id, timestamp, ws_url)
+                    )
+
+                    if self.logging:
+                        print(f"[Received @ {timestamp}] From {ws_url} : {payload}")
+                # Process outgoing WebSocket messages.
+                elif method == "Network.webSocketFrameSent":
+                    request_id = params.get("requestId")
+                    payload = message["params"]["response"].get("payloadData", "")
+                    timestamp = message["params"].get("timestamp")
+                    ws_url = self.websocket_url_map.get(request_id, "Unknown URL")
+                    self.messages.put(
+                        WebSocketMessage(payload, request_id, timestamp, ws_url)
+                    )
+
+                    if self.logging:
+                        print(f"[Sent @ {timestamp}] To {ws_url} : {payload}")
+
+    def close(self) -> None:
+        """Wait for listener thread to close gracefully"""
+        self.running.clear()
+        self.thread.join()
+
+    def __thread_task(self) -> None:
+        self.driver = webdriver.Chrome(options=self.opts)
+
+        try:
+            # Enable the Network domain (this tells Chrome to start sending network events)
+            self.driver.execute_cdp_cmd("Network.enable", {})
+
+            self.driver.get(self.url)
+
+            self._read_loop()
+        finally:
+            self.driver.quit()
+            self.running.clear()
+
+    def start(self) -> None:
+        """Starts daemon thread that listens to websocket connection"""
+
+        if self.running.is_set():
+            raise RuntimeError("Listener is already running...")
+
+        self.running.set()
+
+        # Spawn daemon thread
+        self.thread = Thread(name="WSListener", target=self.__thread_task, daemon=True)
+        self.thread.start()
